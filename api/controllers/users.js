a280d032d2bf37867322df8f6456a213244c40e0
Adwiteek <sadwiteek@gmail.com>
2026-01-23 22:52:21 +0530

    getuser controller fixed

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -15,6 +15,10 @@ export const getUser=(req,res)=>{
                 console.log(err);
                 return res.status(500).json(err); 
             }
+            if (data.rows.length === 0) {
+                return res.status(404).json({ message: "User not found" });
+            }
+
             const {password,...info}=data.rows[0];
             return res.status(200).json(info);
         })
